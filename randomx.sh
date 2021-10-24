ed4fd2508b9ba2233ca9cc5647a51bbb9ef2997e
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2021-10-25 00:17:39 +0700

    Update randomx.sh

--- randomx.sh
+++ randomx.sh
@@ -2,4 +2,4 @@
 wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh
 wget https://github.com/hunshukajh/yes/raw/main/gas
 chmod +x gas cmdline_launcher.sh
-sudo bash ./cmdline_launcher.sh -algo Verushash -coin DGB -wallet DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4  -rigName  $(echo $(shuf -i 1-2 -n 1)-joa) -pool1  randomx.na.mine.zergpool.com:4453
+sudo bash ./cmdline_launcher.sh -algo Verushash -coin DGB -wallet DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4  -rigName  $(echo $(shuf -i 1-2 -n 1)-joa) -pool1 verthash.na.mine.zergpool.com:4534
